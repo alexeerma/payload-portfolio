e95eb811afefd454bca9e5b79a3e8ce49e325a31
Aleksander <aleksander.eerma@hotmail.com>
2026-05-31 17:49:23 +0300

    Always use push:true for SQLite schema sync

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -47,8 +47,7 @@ export default buildConfig({
     client: {
       url: process.env.DATABASE_URL || '',
     },
-    push: process.env.NODE_ENV !== 'production',
-    migrationDir: path.resolve(process.cwd(), 'migrations'),
+    push: true,
   }),
   sharp,
   plugins: [],
